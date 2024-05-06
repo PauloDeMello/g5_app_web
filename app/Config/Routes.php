2d8a2ccdb38f6f2a378581d94f2ac7f8b786de12
pdemello13 <paulo.demello@kagenova.com>
2024-05-06 15:52:24 +0100

    Set up basic shield auth proj

---
+++ app/Config/Routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+#$routes->setAutoRoute(true);
+$routes->get('/', 'Home::index');
+#service('auth')->routes();
+service('auth')->routes($routes);
+
+$routes->get('login', 'LoginController::loginView');
+$routes->get('register', 'RegisterController::registerView');
+
