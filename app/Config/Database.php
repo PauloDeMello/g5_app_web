5fe25ed18a74414aeebbae3c1831b5aaf7129542
PauloDeMello <paulodemello13@gmail.com>
2024-05-28 23:26:00 +0000

    Set up Proj for linux aws env

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'g5dbtest',
+        'database'     => 'g5db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
